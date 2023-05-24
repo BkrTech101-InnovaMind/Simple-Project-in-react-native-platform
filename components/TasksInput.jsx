1be54f14ffcf3f8bf10d4188580429942c325bb9
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-24 21:52:53 +0300

    refactor(tasksinput): add the modal to hide the tasks input and make it in another page which is accessible by the button

--- components/TasksInput.jsx
+++ components/TasksInput.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
-import { Alert, Button, TextInput, View } from "react-native"
+import { Alert, Button, Modal, TextInput, View } from "react-native"
 
-const TasksInput = ({ onAddTask }) => {
+const TasksInput = ({ onAddTask, visible, onCancel }) => {
   const [enteredTask, setEnteredTask] = useState("")
+
   const taskInputHandler = (enteredTask) => {
     setEnteredTask(enteredTask)
   }
+
   const addTaskHandler = () => {
     if (enteredTask.trim() === "") {
       Alert.alert("Task can't be empty")
@@ -16,15 +18,25 @@ const TasksInput = ({ onAddTask }) => {
   }
 
   return (
-    <View className='flex-row flex-1 justify-between items-center mb-6 border-b-2 border-[#cccccc] '>
-      <TextInput
-        onChangeText={taskInputHandler}
-        className='border-2 border-[#cccccc] w-[70%] mr-2 p-2'
-        placeholder='Add Your Task !!'
-        value={enteredTask}
-      />
-      <Button onPress={addTaskHandler} title='Add Task' />
-    </View>
+    <Modal visible={visible} animationType='slide'>
+      <View className='flex-1 justify-center items-center mb-6 border-b-2 border-[#cccccc] p-4'>
+        <TextInput
+          focusable={true}
+          onChangeText={taskInputHandler}
+          className='border-2 border-[#cccccc] w-[70%] p-2'
+          placeholder='Add Your Task !!'
+          value={enteredTask}
+        />
+        <View className='flex-row-reverse top-3'>
+          <View className='w-[100px] mx-2'>
+            <Button onPress={addTaskHandler} title='Add Task' />
+          </View>
+          <View className='w-[100px] mx-2'>
+            <Button title='Cancel' onPress={onCancel} />
+          </View>
+        </View>
+      </View>
+    </Modal>
   )
 }
 
