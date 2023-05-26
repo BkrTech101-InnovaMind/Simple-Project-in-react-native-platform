7fc8f9950038d415a84e8a622b8b15ea40070a96
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-26 16:56:00 +0300

    refactor(app): remove the previous apk from the app file with all the old functionality to impelment the new app using the styled component library

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar"
+
+import { Home } from "./components/Home"
+import { Container } from "./styles/appStyles"
+
+export default function App() {
+  return (
+    <Container>
+      <Home />
+      <StatusBar style='light' />
+    </Container>
+  )
+}
