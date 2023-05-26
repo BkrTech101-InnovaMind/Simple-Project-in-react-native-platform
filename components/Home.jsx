a8aa0fde78e2a39cf51c293db26b67bc29442348
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-26 21:25:43 +0300

    refactor(home): implement the necesary functionality to save the tasks in the storage t onever been deleted when exit the app

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { useEffect, useState } from "react"
 import Header from "./Header"
 import ListItems from "./ListItems"
 import ToDosInput from "./ToDosInput"
@@ -13,14 +14,41 @@ export default function Home() {
 
   const [inputValue, setInputValue] = useState()
 
-  const handleClearToDos = () => {
-    setToDos([])
+  useEffect(() => {
+    loadTasksFromStorage()
+  }, [])
+
+  const loadTasksFromStorage = async () => {
+    try {
+      const storedTasks = await AsyncStorage.getItem("tasks")
+      if (storedTasks !== null) {
+        setToDos(JSON.parse(storedTasks))
+      }
+    } catch (error) {
+      console.error("Error loading tasks from storage:", error)
+    }
   }
 
-  const handleNewToDos = (toDo) => {
+  const handleClearToDos = async () => {
+    setToDos([])
+
+    try {
+      await AsyncStorage.setItem("tasks", JSON.stringify([]))
+    } catch (error) {
+      console.error("Error saving tasks to storage:", error)
+    }
+  }
+
+  const handleNewToDos = async (toDo) => {
     const newToDos = [...toDos, toDo]
     setToDos(newToDos)
     setInputVisibility(false)
+
+    try {
+      await AsyncStorage.setItem("tasks", JSON.stringify(newToDos))
+    } catch (error) {
+      console.error("Error saving tasks to storage:", error)
+    }
   }
 
   return (
